88b398de4b63671421cf52f946b6112c38423548
Oleksii Kudriavtsev <Lesha61945@gmail.com>
2024-09-14 13:19:02 +0300

    make build

---
+++ index.4ca8cda6.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){var r=e.target.closest(".list-item__link");e.preventDefault(),t.src=r});
+//# sourceMappingURL=index.4ca8cda6.js.map
